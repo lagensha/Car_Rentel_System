6f73dd76cad16bbc9e4ad80ca8a99631e422a2b3
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-03-18 10:58:10 +0530

    Add CarCatalog component with template and styles

---
+++ src/app/car-catalog/car-catalog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-catalog',
+  imports: [],
+  templateUrl: './car-catalog.html',
+  styleUrl: './car-catalog.css',
+})
+export class CarCatalog {}
